ea83d9549cb44cb05a4c25968dd1741064ceaad0
Kamil <kamil.slosarczyk@yahoo.com>
2020-03-31 16:33:32 +0100

    Project in HTML, JavaScript, CSS and Cloud Firestore Database.
    Lists Added book to a database. Also lets us to add new positions.

---
+++ app.js
@@ -0,0 +1,71 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAMu_JIQpYHQUhDT4_EnG7sdQqV6LrOW9E",
+    authDomain: "favouritebook-5a724.firebaseapp.com",
+    databaseURL: "https://favouritebook-5a724.firebaseio.com",
+    projectId: "favouritebook-5a724",
+    storageBucket: "favouritebook-5a724.appspot.com",
+    messagingSenderId: "1064227546027",
+    appId: "1:1064227546027:web:b3b8bda919ef7389ca28ee",
+    measurementId: "G-LVVQ40N35G"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+
+const bookList = document.querySelector('#book-list');
+const form = document.querySelector('#add-book');
+
+
+function renderBook(doc){
+    let li = document.createElement('li');
+    let Name = document.createElement('span');
+    let Surname = document.createElement('span');
+    let Title = document.createElement('span');
+    let Genre = document.createElement('span');
+    let Published = document.createElement('span');
+    let ISBN = document.createElement('span');
+
+    let cross = document.createElement('div');
+
+    li.setAttribute('data-id', doc.id);
+    Name.textContent = doc.data().Name;
+    Surname.textContent = doc.data().Surname;
+    Title.textContent = doc.data().Title;
+    Genre.textContent = doc.data().Genre;
+    Published.textContent = doc.data().Published;
+    ISBN.textContent = doc.data().ISBN;
+    cross.textContent = 'x';
+
+    li.appendChild(Name);
+    li.appendChild(Surname);
+    li.appendChild(Title);
+    li.appendChild(Genre);
+    li.appendChild(Published);
+    li.appendChild(ISBN);
+    li.appendChild(cross);
+
+    bookList.appendChild(li);
+
+}
+//Get Data from DataBase
+db.collection('Books').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        renderBook(doc);
+        }
+    )
+})
+//Save Data to DataBase
+form.addEventListener('submit', (e)=> {
+    e.preventDefault(); //Page doesnt refresh after submitting text
+    db.collection('Books').add({
+        Name: form.Name.value,
+        Surname: form.Surname.value,
+        Title: form.Title.value,
+        Genre: form.Genre.value,
+        Published: form.Published.value,
+        ISBN: form.ISBN.value,
+    })
+    form.reset(); //full reset of items above after submitting
+})
